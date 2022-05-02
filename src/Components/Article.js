353b9ff711597f40ba69f5fe1bbe7dc381f55224
Eddavtyan <edgar.davtyan@start49.com>
2022-05-02 13:29:38 +0400

    CVtemplateWithFunctionality

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,13 +1,40 @@
 import React from "react";
 import '../CV.css';
-import { Grid, Container, Typography } from '@mui/material';
+import { Grid, Container, Typography, Box } from '@mui/material';
 
 
-export function Article({topic, subtopic, secondary_text}) {
+export function Article({topic, subtopic, secondarySubtopic, date, secondaryText, location}) {
+    let secText;
+
+    if (secondaryText !== undefined)
+        if (Array.isArray(secondaryText)){
+            secText = secondaryText.map((itm, index) => {
+                return (
+                    <ul className="text-list" key={index}>
+                        <li>{itm}</li>
+                    </ul>
+                );
+            });
+        }else{
+            secText = <p className="text-secondary">{secondaryText}</p>;
+        }
+
     return (
-        <Grid item xs={6}>
+        <>
+            {topic !== undefined && <h3 className="topic">{topic}</h3>}
             {subtopic !== undefined && <h3 className="sub-topic">{subtopic}</h3>}
-            {secondary_text !== undefined && <p className="text-secondary">{secondary_text}</p>}
-        </Grid>
+            {secondarySubtopic !== undefined &&
+                <Box className="sub-topic-secondary">
+                    <h2>{secondarySubtopic}</h2>
+                    {date !== undefined && <h3>{date}</h3>}
+                </Box>}
+            {secondaryText !== undefined && secText}
+            {location !== undefined &&
+                <Box className="location">
+                    <span className="material-icons">location_on</span>
+                    <h3>{location}</h3>
+                </Box>
+            }
+        </>
     );
 }
--- src/Components/CvTemplate.js
+++ src/Components/CvTemplate.js
@@ -1,100 +1,163 @@
 import React from "react";
 import '../CV.css';
-import { Grid, Container, Typography } from '@mui/material';
+import { Grid, Container, Typography, Box } from '@mui/material';
 import 'material-icons/iconfont/material-icons.css';
 import logoPng from '../images/img.png'
 import textPng from '../images/img_1.png'
 import leftSectionPng from '../images/img_2.png'
-import watermark from '../images/img_3.png'
 import {Article} from "./Article";
+
+import {dataTest} from "./dataForTest";
 
 
 export function CvTemplate()
 {
+    const header = dataTest.header;
+    const main = dataTest.main.content;
+
+    let contentRow = [];
+    let contentSection = [];
+
+    main.forEach((itm, index) => {
+        let content = [];
+        const body = [].concat(itm.body);
+        let contentType = itm.body.length > 2;
+
+        body.forEach((it, ind) => {
+            content.push(contentType ? <Grid item xs={6} key={ind}><Article {...it}/></Grid> : <Article {...it} key={ind}/>);
+        });
+
+        if (contentType){
+            contentSection.push(
+                <Grid item key={index}>
+                    <Grid item xs={12}>
+                        <Box>
+                            <Article topic={itm.topic}/>
+                        </Box>
+                    </Grid>
+                    <Grid container item columnSpacing={{xs: 6}}>
+                        {content}
+                    </Grid>
+                </Grid>
+            );
+        }else{
+            contentRow.push(
+                <Grid item key={index}>
+                    <Article topic={itm.topic}/>
+                    {content}
+                </Grid>
+            );
+        }
+    });
+
+    const imagePerson = "url(\"data:image/png;base64," + header.image + "\")";
 
     return (
-      <div className="mainContainer">
-          <div className="watermark">
-              <img width="100%" src={watermark} alt="#"/>
-          </div>
-          <div>
+      <Box className="mainContainer">
+          <Box>
               <img src={leftSectionPng} className="header-background" alt="#"/>
-              <Grid container sx={{height: "100%", marginTop: "50px"}} direction="row" alignItems="top"
+              <Grid container mt="50px" direction="row" alignItems="top"
                     justifyContent="space-between">
-                  <Grid item display="flex" alignItems="center" sx={{marginLeft: "178px", marginTop: "56px"}}>
-                      <div style={{width: 191, height: 191, borderRadius: "50%", backgroundColor: "blue"}}>
-                          {/*<img src="" alt=""/>*/}
-                      </div>
-                      <div style={{paddingLeft: 25, marginTop: -30}}>
-                          <Grid>
-                              <p className="header-text textAlignLeft" style={{fontSize: 36, fontWeight: 900}}>Abraham Hayrapetyan</p>
-                          </Grid>
-                          <Grid sx={{marginTop: "8px"}}>
-                              <p className="header-text textAlignLeft" style={{fontWeight: 300}}>Senior Flutter Developer</p>
-                          </Grid>
-                      </div>
+                  <Grid item display="flex" alignItems="center" ml="182px" mt="62px">
+                      <Box className="image-container" style={{backgroundImage : imagePerson}}>
+                      </Box>
+                      <Box pl="25px" mb="35px">
+                          <Box>
+                              <Typography fontSize="36px" fontWeight="900" className="header-text textAlignLeft" >{header.fname + " " + header.lname}</Typography>
+                          </Box>
+                          <Box>
+                              <Typography fontSize="16px" fontWeight="300" className="header-text textAlignLeft">{header.position}</Typography>
+                          </Box>
+                      </Box>
                   </Grid>
-                  <Grid item sx={{marginRight: "50px"}}>
-                      <Grid display="flex" justifyContent="right" alignItems="center">
-                          <div style={{height: 68, marginRight: 15}}>
+                  <Grid item mr="50px">
+                      <Box display="flex" justifyContent="right" alignItems="center">
+                          <Box height="68px" mr="15px">
                               <img src={logoPng} alt="#" height="100%"/>
-                          </div>
-                          <div style={{height: 30}}>
+                          </Box>
+                          <Box height="30px">
                               <img src={textPng} alt="#" height="100%"/>
-                          </div>
-                      </Grid>
-                      <Grid sx={{paddingTop: "30px"}}>
-                          <Grid>
-                              <p className="header-text textAlignRight"><span style={{fontSize: "18px"}}
-                                                                              className="material-icons">location_on</span> Armenia, Ghukasavan </p>
-                          </Grid>
-                          <Grid sx={{marginTop: "10px"}}>
-                              <p className="header-text textAlignRight" style={{fontWeight: 500}}><span
+                          </Box>
+                      </Box>
+                      <Box pt="30px">
+                          <Box>
+                              <Typography className="header-text textAlignRight"><span style={{fontSize: "18px"}} className="material-icons">location_on</span>{header.location}</Typography>
+                          </Box>
+                          <Box mt="10px">
+                              <Typography className="header-text textAlignRight" fontWeight="500"><span
                                   style={{fontSize: "18px", transform: "rotate(-45deg)"}}
-                                  className="material-icons">send</span> <b>Email</b>: candidate+abrahamyan@start49.com
-                              </p>
-                          </Grid>
-                      </Grid>
+                                  className="material-icons">send</span> <b>Email:</b> {header.email}
+                              </Typography>
+                          </Box>
+                      </Box>
                   </Grid>
               </Grid>
-          </div>
-          <Grid container sx={{marginTop : "38px", padding : "0 100px"}} columnSpacing={{xs : 6}} justifyContent="space-between" direction="row" alignItems="top">
-              <Grid item xs={4}>
-                  <h3 className="topic">Summary</h3>
-                  <p className="text-secondary">
-                      I have been working as a software developer for more than 6 years with about 25 completed projects in my portfolio. The most known of projects in my portfolio are Volterman, Smart push, Qartez, GG, First aid Armenia, Cinema star, FCBank etc. Other big projects are for Hotel and Restaurant Management. Most of my experience is in mobile application development, but I am also experienced in TV application and Backend development.
-                  </p>
+          </Box>
+
+          {/*----------------------Content----------------------*/}
+
+          <Grid container mt="38px" p="0 100px" columnSpacing={{xs : 6}} justifyContent="space-between" direction="row" alignItems="top">
+              <Grid item container xs={4} direction="column" rowSpacing={{xs:8}}>
+                  {contentRow}
               </Grid>
-              <Grid container alignItems="top" item xs={8}>
-                  <Grid item xs={12}>
-                      <h3 className="topic">Experience</h3>
-                  </Grid>
-                  <Grid container item columnSpacing={{xs: 6}}>
-                      <Article subtopic="IMEASYSTEMS" secondary_text="I have been working as a software developer for more than 6 years with about 25 completed
-                              projects in my portfolio. The most known of projects in my portfolio are Volterman, Smart
-                              push, Qartez, GG, First aid Armenia, Cinema star, FCBank etc. Other big projects are for Hotel
-                              and Restaurant Management. Most of my experience is in mobile application development, but I
-                              am also experienced in TV application and Backend development."/>
-                      <Article secondary_text="I have been working as a software developer for more than 6 years with about 25 completed
-                              projects in my portfolio. The most known of projects in my portfolio are Volterman, Smart
-                              push, Qartez, GG, First aid Armenia, Cinema star, FCBank etc. Other big projects are for Hotel
-                              and Restaurant Management. Most of my experience is in mobile application development, but I
-                              am also experienced in TV application and Backend development."/>
-                  </Grid>
+              {/*-----------------*/}
+              <Grid item container xs={8} direction="column" rowSpacing={{xs:8}}>
+                  {contentSection}
               </Grid>
-              {/*<Grid item xs={4}>*/}
-              {/*    <h3 className="topic">Summary</h3>*/}
-              {/*    <p className="text-secondary">*/}
-              {/*        I have been working as a software developer for more than 6 years with about 25 completed projects in my portfolio. The most known of projects in my portfolio are Volterman, Smart push, Qartez, GG, First aid Armenia, Cinema star, FCBank etc. Other big projects are for Hotel and Restaurant Management. Most of my experience is in mobile application development, but I am also experienced in TV application and Backend development.*/}
-              {/*    </p>*/}
-              {/*</Grid>*/}
-              {/*<Grid item xs={4}>*/}
-              {/*    <h3 className="topic">Summary</h3>*/}
-              {/*    <p className="text-secondary">*/}
-              {/*        I have been working as a software developer for more than 6 years with about 25 completed projects in my portfolio. The most known of projects in my portfolio are Volterman, Smart push, Qartez, GG, First aid Armenia, Cinema star, FCBank etc. Other big projects are for Hotel and Restaurant Management. Most of my experience is in mobile application development, but I am also experienced in TV application and Backend development.*/}
-              {/*    </p>*/}
-              {/*</Grid>*/}
           </Grid>
-      </div>
+      </Box>
+
+
+    // <Grid item>
+    //     <Grid item xs={12}>
+    //         <Box>
+    //             <h3 className="topic">Experience</h3>
+    //         </Box>
+    //     </Grid>
+    //     <Grid container item columnSpacing={{xs: 6}}>
+    //         <Grid item xs={6}>
+    //             <h3 className="sub-topic">subtopic</h3>
+    //             <Box className="sub-topic-secondary">
+    //                 <h2>Mobile Team Lead (Flutter Developer)</h2>
+    //                 <h3>2021 May - Present</h3>
+    //             </Box>
+    //             <p className="text-secondary">
+    //                 I have been working as a software developer for more than 6 years with about 25 completed
+    //                 projects in my portfolio. The most known of projects in my portfolio are Volterman, Smart
+    //                 push, Qartez, GG, First aid Armenia, Cinema star, FCBank etc. Other big projects are for
+    //                 Hotel and Restaurant Management. Most of my experience is in mobile application
+    //                 development, but I am also experienced in TV application and Backend development.
+    //             </p>
+    //             <ul className="text-list">
+    //                 <li>Formal Verification of partitioned netlists represented in Xilinx EDIF format. </li>
+    //             </ul>
+    //             <Box className="location">
+    //                 <span className="material-icons">location_on</span>
+    //                 <h3>Yerevan, Armenia</h3>
+    //             </Box>
+    //         </Grid>
+    //         <Grid item xs={6}>
+    //             <h3 className="sub-topic">subtopic</h3>
+    //             <Box className="sub-topic-secondary">
+    //                 <h2>Mobile Team Lead (Flutter Developer)</h2>
+    //                 <h3>2021 May - Present</h3>
+    //             </Box>
+    //             <p className="text-secondary">
+    //                 I have been working as a software developer for more than 6 years with about 25 completed
+    //                 projects in my portfolio. The most known of projects in my portfolio are Volterman, Smart
+    //                 push, Qartez, GG, First aid Armenia, Cinema star, FCBank etc. Other big projects are for
+    //                 Hotel and Restaurant Management. Most of my experience is in mobile application
+    //                 development, but I am also experienced in TV application and Backend development.
+    //             </p>
+    //             <ul className="text-list">
+    //                 <li>Formal Verification of partitioned netlists represented in Xilinx EDIF format. </li>
+    //             </ul>
+    //             <Box className="location">
+    //                 <span className="material-icons">location_on</span>
+    //                 <h3>Yerevan, Armenia</h3>
+    //             </Box>
+    //         </Grid>
+    //     </Grid>
+    // </Grid>
     );
 }
