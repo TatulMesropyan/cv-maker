4124d015e3fc0c2e16ce5d9ae07e9a187b6425a5
TatulMesropyan <tatul.mesropyan@gmail.com>
2022-05-19 14:00:30 +0400

    Finilized conflicts in git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Form from './Formcreator/Form/Form';
+import CvTemplate from "./Components/CvTemplate"
+import './App.css';
+function App(store) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Form />}/>
+        <Route path="/cv" exact element={<CvTemplate />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
