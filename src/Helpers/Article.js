3f86103e6da7fb5cf8f33071cc774dcac12cb29a
Eddavtyan <edgar.davtyan@start49.com>
2022-05-20 16:40:16 +0400

    added languages on cvTemplate

--- src/Helpers/Article.js
+++ src/Helpers/Article.js
@@ -1,40 +1,53 @@
 import React from "react";
-import '../CV.css';
 import { Box } from '@mui/material';
+import LanguagePacket from "./languages.json";
+import '../CV.css';
 
 
-export function Article({topic, subtopic, secondarySubtopic, date, secondaryText, location}) {
+export function Article({topic, subtopic, secondarySubtopic, date, languages, secondaryText, location}) {
     let secText;
 
-    if (secondaryText)
-        if (Array.isArray(secondaryText)) {
-            secText = secondaryText.map((itm, index) => {
-                return (
-                    <ul className="text-list" key={index}>
-                        <li>{itm}</li>
-                    </ul>
-                );
-            });
-        } else {
-            secText = <p className="text-secondary">{secondaryText}</p>;
-        }
+		if (secondaryText)
+			if (Array.isArray(secondaryText)) {
+				secText = secondaryText.map((itm, index) => {
+					return (
+						<ul className="text-list" key={index}>
+							<li>{itm}</li>
+						</ul>
+					);
+				});
+			} else {
+				secText = <p className="text-secondary">{secondaryText}</p>;
+			}
 
-    return (
-        <>
-            {topic && <h3 className="topic">{topic}</h3>}
-            {subtopic && <h3 className="sub-topic">{subtopic}</h3>}
-            {secondarySubtopic &&
-                <Box className="sub-topic-secondary">
-                    <h2>{secondarySubtopic}</h2>
-                    {date && <h3>{date}</h3>}
-                </Box>}
-            {secondaryText && secText}
-            {location &&
-                <Box className="location">
-                    <span className="material-icons">location_on</span>
-                    <h3>{location}</h3>
-                </Box>
-            }
-        </>
-    );
+		return (
+			<>
+				{topic && <h3 className="topic">{topic}</h3>}
+				{subtopic && <h3 className="sub-topic">{subtopic}</h3>}
+				{secondarySubtopic && (
+					<Box className="sub-topic-secondary">
+						<h2>{secondarySubtopic}</h2>
+						{date && <h3>{date}</h3>}
+					</Box>
+				)}
+				{secondaryText && secText}
+				{languages &&
+					languages.map((v, i) => {
+						if (v) {
+							return (
+								<Box key={i} className="Languages">
+									{LanguagePacket[v].emoji}
+									{LanguagePacket[v].name}
+								</Box>
+							);
+						}
+					})}
+				{location && (
+					<Box className="location">
+						<span className="material-icons">location_on</span>
+						<h3>{location}</h3>
+					</Box>
+				)}
+			</>
+		);
 }
