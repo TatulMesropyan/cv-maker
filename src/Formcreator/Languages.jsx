248e0bbe12169a377045ea715b5c6de293472221
TatulMesropyan <tatul.mesropyan@gmail.com>
2022-05-20 15:12:40 +0400

    Featuring redux to language form

--- src/Formcreator/Languages.jsx
+++ src/Formcreator/Languages.jsx
@@ -1,8 +1,8 @@
-import labels from "../Components/labels";
+import labels from "../Helpers/labels"
 import * as actions from "../redux/Actions/languageActions";
 import { useSelector, useDispatch } from "react-redux";
-import languageDropdown from "../Components/LangDropdown";
-import languagePacket from "../Assets/languages.json";
+import languageDropdown from "../Helpers/LangDropdown";
+import languagePacket from "../Helpers/languages.json";
 import "../App.css";
 import {
   TextField,
@@ -14,17 +14,11 @@ import {
   CardContent,
 } from "@mui/material";
 import { useCallback } from "react";
-import Checkbox from '@mui/material/Checkbox';
 
 
-const Languages = () => {
+const Languages = ({getData,topic}) => {
   const dispatch = useDispatch();
 
-  const { languageRedux } = useSelector((state) => state.languageDataReducer);
-
-  const handleAddClick = useCallback(() => {
-    dispatch(actions.languageAdd());
-  }, [dispatch]);
 
   const handleOnRemove = (e, index) => {
     dispatch(actions.languageRemove(index));
@@ -52,100 +46,7 @@ const Languages = () => {
           >
             {labels.language}
           </Typography>
-          <LanguageDropdown />
-          <Grid sx={{ p: 2 }}>
-            <Button
-              variant="contained"
-              color="success"
-              onClick={handleAddClick}
-            >
-              {labels.addButton}
-            </Button>
-            <Typography
-              gutterBottom={true}
-              variant="h5"
-              align="center"
-              sx={{ fontStyle: "italic" }}
-            >
-              Proficiency level
-            </Typography>
-            <Grid>
-            <Typography
-              gutterBottom={true}
-              variant="h7"
-              align="center"
-              sx={{ fontStyle: "italic" }}
-            >
-              Low Proficiency
-            </Typography>
-            </Grid>
-            <Grid>
-            <Checkbox/>
-            </Grid>
-            <Grid>
-            <Typography
-              gutterBottom={true}
-              variant="h7"
-              align="center"
-              sx={{ fontStyle: "italic" }}
-            >
-              Elementary Proficiency
-            </Typography>
-            </Grid>
-            <Grid>
-            <Checkbox/>
-            </Grid>
-            <Grid>
-            <Typography
-              gutterBottom={true}
-              variant="h7"
-              align="center"
-              sx={{ fontStyle: "italic" }}
-            >
-              Limited Working Proficiency.
-            </Typography>
-            </Grid>
-            <Grid>
-            <Checkbox/>
-            </Grid>
-            <Grid>
-            <Typography
-              gutterBottom={true}
-              variant="h7"
-              align="center"
-              sx={{ fontStyle: "italic" }}
-            >
-              Professional Working Proficiency
-            </Typography>
-            </Grid>
-            <Grid>
-            <Checkbox/>
-            </Grid>
-            <Grid>
-            <Typography
-              gutterBottom={true}
-              variant="h7"
-              align="center"
-              sx={{ fontStyle: "italic" }}
-            >
-              Native Proficiency
-            </Typography>
-            </Grid>
-            <Grid>
-            <Checkbox/>
-            </Grid>
-          </Grid>
-          {languageRedux.map((name, index) => (
-            <div key={index}>
-              <Grid sx={{ p: 2 }}>
-                <TextField
-                  disabled
-                  variant="filled"
-                  // value={language.emoji ? ` ${language.emoji}  ${name}` : name}
-                  value={name}
-                />
-                <Grid></Grid>
-              </Grid>
+          <LanguageDropdown getData={getData} topic={topic}/>
               <Grid>
                 <Button
                   variant="contained"
@@ -155,8 +56,6 @@ const Languages = () => {
                   {labels.removeButton}
                 </Button>
               </Grid>
-            </div>
-          ))}
         </CardContent>
       </Card>
     </Box>
